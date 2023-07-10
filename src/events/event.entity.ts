f53e2e4ec43a5b38b66fc7db259dfc316ea65294
Robby Widyahartono <robby.widyahartono@gmail.com>
2023-07-10 12:51:46 +0700

    feature: integrate with database

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -1,7 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ length: 100 })
   name: string;
+  @Column()
   description: string;
+  @Column()
   when: Date;
+  @Column()
   address: string;
 }
--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -11,42 +11,70 @@ import {
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
 import { Event } from './event.entity';
+import { Like, MoreThan, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller('events')
 export class EventsController {
-  private events: Event[] = [];
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventRepository: Repository<Event>,
+  ) {}
+
   @Get()
-  findAll() {
-    return this.events;
+  async findAll() {
+    return await this.eventRepository.find();
   }
+
+  @Get('/practice')
+  async practice() {
+    return await this.eventRepository.find({
+      select: ['id', 'name'],
+      where: [
+        {
+          id: MoreThan(3),
+          when: MoreThan(new Date('2021-02-12T13:00:00')),
+        },
+        {
+          description: Like('%meet%'),
+        },
+      ],
+      take: 2,
+      order: {
+        id: 'DESC',
+      },
+    });
+  }
+
   @Get(':id')
-  findOne(@Param('id') id) {
-    const event = this.events.find((event) => event.id === parseInt(id));
+  async findOne(@Param('id') id) {
+    const event = await this.eventRepository.findOne(id);
     return event;
   }
+
   @Post()
-  create(@Body() input: CreateEventDto) {
+  async create(@Body() input: CreateEventDto) {
     const event = {
       ...input,
       when: new Date(input.when),
-      id: this.events.length + 1,
     };
-    this.events.push(event);
-    return event;
+    return await this.eventRepository.save(event);
   }
+
   @Patch(':id')
-  update(@Param('id') id, @Body() input: UpdateEventDto) {
-    const index = this.events.findIndex((event) => event.id === parseInt(id));
-    this.events[index] = {
-      ...this.events[index],
+  async update(@Param('id') id, @Body() input: UpdateEventDto) {
+    const event = await this.eventRepository.findOne(id);
+    return await this.eventRepository.save({
+      ...event,
       ...input,
-      when: input.when ? new Date(input.when) : this.events[index].when,
-    };
-    return this.events[index];
+      when: input.when ? new Date(input.when) : event.when,
+    });
   }
+
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id) {
-    this.events = this.events.filter((event) => event.id !== parseInt(id));
+  async remove(@Param('id') id) {
+    const event = await this.eventRepository.findOne(id);
+    return await this.eventRepository.remove(event);
   }
 }
